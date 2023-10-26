5af37f7f0ef7a12e4a4f4bd20f413370f3b93b57
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-10-26 15:58:16 -0300

    feat: create get  unit servic

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { GetUnitsService } from 'src/app/services/get-units.service';
 
 @Component({
   selector: 'app-forms',
@@ -10,9 +11,13 @@ export class FormsComponent {
   filteredResults: string[] = [];
   formGroup!: FormGroup;
 
-  constructor( private formBuilder:FormBuilder){}
+  constructor( 
+    private formBuilder:FormBuilder,
+    private unitService: GetUnitsService
+    ){}
   
   ngOnInit():void {
+    this.unitService.getAllUnits().subscribe( data => {console.log(data);})
     this.formGroup = this.formBuilder.group({
       hour: '',
       showClosed: true
