b6dcb178c9574a6bdbde8d32ff9c8b52786a587f
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-10-30 14:29:22 -0300

    feat: create unit cards list

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FilterUnitsService } from 'src/app/services/filter-units.service';
 import { GetUnitsService } from 'src/app/services/get-units.service';
@@ -10,6 +10,8 @@ import { Location } from 'src/app/types/location.interface';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent {
+  @Output() submitEvent = new EventEmitter();
+
   results: Location[] = [];
   filteredResults: Location[] = [];
   formGroup!: FormGroup;
@@ -35,6 +37,8 @@ export class FormsComponent {
     let {showClosed, hour } = this.formGroup.value;
     this.filteredResults = this.filterUnitService.filter(this.results, showClosed, hour);
     this.unitService.setFilteredUnit(this.filteredResults);
+    
+    this.submitEvent.emit();
   }
   onClean(): void {
     this.formGroup.reset();
