7e71e60836325f953c03d7ca5773d9903abec0b4
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-10-30 14:29:43 -0300

    feat: add card list to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Location } from './types/location.interface';
+import { GetUnitsService } from './services/get-units.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-smartfit';
+  showList = new BehaviorSubject(false);
+  unitsList: Location[] = [];
+
+  constructor(private unitService: GetUnitsService){}
+
+  onSubmit(){
+    this.unitsList = this.unitService.getFilteredUnits(); 
+    this.showList.next(true);
+  }
 }
